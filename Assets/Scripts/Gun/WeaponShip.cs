c43b533694e73ce7958691f941100a3ecbe6cad0
dadoczek <adriandadok.dadoczek@gmail.com>
2021-03-14 19:39:46 +0900

    wybór broni

--- Assets/Scripts/Gun/WeaponShip.cs
+++ Assets/Scripts/Gun/WeaponShip.cs
@@ -1,53 +1,61 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class WeaponShip : MonoBehaviour
 {
-    public GameObject bullet;
-    public GameObject bullet1;
+    public GameObject defaultWeapon;
+    public List<GameObject> prefubWeapons = new List<GameObject>();
     private GameObject clone;
+
     //
-    public GameObject yellowbullet;
-    //  
+    public GameObject currentWeapon;
+
+
+    public Dictionary<string, GameObject> weapons = new Dictionary<string, GameObject>();
+ 
     int weaponNumber = 1;
+    
+    void Start()
+    {
+        SetDefaultWeapon();
+    }
+
     private void Update()
     {
-
-        switch (weaponNumber)
+        if (Input.GetButtonDown("Fire1"))
         {
-            case 1:
-                if (Input.GetButtonDown("Fire1"))
-                {
-                    clone = Instantiate(bullet, transform.position, Quaternion.identity);
-                    if (PlayerStatss.Instance.greenPoints >= 1)
-                    {
-                        clone = Instantiate(bullet1, transform.position, Quaternion.identity);
-                    }
-                    else if (PlayerStatss.Instance.yellowPoints >= 1)
-                    {
-                        clone = Instantiate(yellowbullet, transform.position, Quaternion.identity);
-                    }
-
-
-                }
-                break;
-            case 2:
-                if (PlayerStatss.Instance.yellowPoints >= 1)
-                {
-                    if (Input.GetButtonDown("Fire1"))
-                    {
-                        clone = Instantiate(bullet, transform.position, Quaternion.identity);
-                        clone = Instantiate(yellowbullet, transform.position, Quaternion.identity);
-                    }
-
-                }
-                break;
-            default:
-                Debug.Log("powrot");
-                break;
+            clone = Instantiate(currentWeapon, transform.position, Quaternion.identity);
         }
-        
+    }
 
+    public void SetDefaultWeapon()
+    {
+        currentWeapon = defaultWeapon;
+        foreach (var weapon in weapons.Where(x => x.Value.tag != defaultWeapon.tag))
+        {
+            Destroy(weapon.Value);
+        }
+
+        weapons.Clear();
+        weapons.Add(defaultWeapon.tag, defaultWeapon);
+    }
+
+    internal void AddWeapon(string tag)
+    {
+        if (!weapons.TryGetValue(tag, out var newWapon))
+        {
+            var weaponPrefab = prefubWeapons.FirstOrDefault(x => x.tag == tag);
+            if (weaponPrefab == null)
+            {
+                throw new Exception("Brak rodzaju broni w deklarowanych prefabach");
+            }
+
+            newWapon = weaponPrefab;
+            weapons.Add(tag, newWapon);
+        }
+        currentWeapon = newWapon;
     }
 }
