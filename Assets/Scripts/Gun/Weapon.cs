acde50a8a92f5e117cc8e7fca39dc96b9b88d333
AdusV <adriankomendera@interia.pl>
2021-03-14 11:50:47 +0100

    New

---
+++ Assets/Scripts/Gun/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            var weaponShip = other.GetComponent<WeaponShip>();
+            weaponShip.AddWeapon(gameObject.tag);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
