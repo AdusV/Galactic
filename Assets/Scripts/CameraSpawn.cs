5d31645df68a65450f9af25fc9174d1223fbe55d
dadoczek <adriandadok.dadoczek@gmail.com>
2021-04-25 20:15:53 +0900

    rozmiar okna

--- Assets/Scripts/CameraSpawn.cs
+++ Assets/Scripts/CameraSpawn.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.ProBuilder;
+using Math = System.Math;
 
 public class CameraSpawn : MonoBehaviour
 {
-    public GameObject ball;
+    public GameObject ball; 
+    
     
     private void Start()
     {
@@ -12,7 +15,34 @@ public class CameraSpawn : MonoBehaviour
     }
     public void Spawn()
     {
-        var camera = Camera.main.transform;
-        Instantiate(ball, camera.position, Quaternion.identity);
+        var camera = Camera.main;
+        var angle = camera.fieldOfView;
+        
+        var width = camera.pixelWidth / 2;
+        var height = camera.pixelHeight / 2;
+
+        var sum = width + height;
+
+        var c = (double)width / sum;
+        var xx = c * 1.5708;
+        var w = Math.Tan(xx) * 8;
+        
+        
+        var cc = (double)height / sum;
+        var x = cc * 1.5708;
+        var h = Math.Tan(x) * 8;
+        
+        
+        Debug.Log(h);
+        Debug.Log(w);
+        
+        
+        
+
+            
+
+
+
+        Instantiate(ball, camera.transform.position, Quaternion.identity);
     }
 }
